63c89055aea909e575290fc21b05b6cc641e29cb
woo <wooomr2@gmail.com>
2022-05-31 14:54:13 +0900

    css added

--- client/src/pages/categories/Category.jsx
+++ client/src/pages/categories/Category.jsx
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductsByCategories } from "../../slice/productSlice";
 import publicURL from "../../utils/publicURL";
-import { categoryToggle, createLinearCategory } from "../../slice/categorySlice";
+import {
+  categoryToggle,
+  createLinearCategory,
+} from "../../slice/categorySlice";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Paging from "../../components/paging/Paging";
 import FilterListIcon from "@mui/icons-material/FilterList";
@@ -23,9 +26,9 @@ function Category() {
   const currentCategory = [];
   const [sort, onChangeSort] = useInput(_sort);
   let cids = [];
-  
+
   if (params.cid === "all") {
-    cids=[];
+    cids = [];
   } else {
     function findCategory(categories) {
       if (currentCategory.length > 0) return;
@@ -56,7 +59,6 @@ function Category() {
     dispatch(categoryToggle()); // 카테고리 토글
   };
 
-
   const onClickNavigate = useCallback(
     (page) => () => {
       navigate(page);
@@ -64,49 +66,53 @@ function Category() {
     []
   );
 
-
   return (
     <main className="categories-container">
-    <Sidebar
-      brandData={brandData}
-      brands={brands}
-      setBrands={setBrands}
-      setCurrentPage={setCurrentPage}
-      categoryOpen={categoryOpen}
-      categoryToggleHandler={categoryToggleHandler}
-    />
-    <section className="products-container">
-      <div className="filter-wrapper">
-        <div className="filter-item" onClick={categoryToggleHandler}>
-          <FilterListIcon className="filter-icon" />
-          <span>FILTER</span>
-        </div>
-        <div className="sort-item">
-          <select onChange={onChangeSort}>
-            <option defaultValue hidden>
-              SORT
-            </option>
-            <option value={"timestamps"}>신상품</option>
-            <option value={"ascending"}>낮은가격</option>
-            <option value={"descending"}>높은가격</option>
-          </select>
-        </div>
-      </div>
-      <div className="products-wrapper">
-        {/* {products?.map((product) => (
-          <div
-            className="products-items"
-            key={product._id}
-            onClick={onClickNavigate(`/products/${product._id}`)}
-          >
-            <img src={publicURL(product.productImgs[0].fileName)} alt="" />
-            <p>{product.name}</p>
+      <Sidebar
+        brandData={brandData}
+        brands={brands}
+        setBrands={setBrands}
+        setCurrentPage={setCurrentPage}
+        categoryOpen={categoryOpen}
+        categoryToggleHandler={categoryToggleHandler}
+      />
+      <section className="products-container">
+        <div className="filter-wrapper">
+          <div className="filter-item" onClick={categoryToggleHandler}>
+            <FilterListIcon className="filter-icon" />
+            <span>FILTER</span>
           </div>
-        ))} */}
-        products.map(product=> (<ProductItem product={product}/>))
-      </div>
-    </section>
-  </main>
+          <div className="sort-item">
+            <select onChange={onChangeSort}>
+              <option defaultValue hidden>
+                SORT
+              </option>
+              <option value={"timestamps"}>신상품</option>
+              <option value={"ascending"}>낮은가격</option>
+              <option value={"descending"}>높은가격</option>
+            </select>
+          </div>
+        </div>
+        <div className="products-wrapper">
+          {products?.map((product) => (
+            <div
+              className="products-items"
+              key={product._id}
+              onClick={onClickNavigate(`/products/${product._id}`)}
+            >
+              <img src={publicURL(product.productImgs[0].fileName)} alt="" />
+              <p>{product.name}</p>
+            </div>
+          ))}
+        </div>
+        <Paging
+          total={total}
+          perPage={perPage}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </section>
+    </main>
   );
 }
 
